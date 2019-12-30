8f02a01e3689d64a1ad9bbc1dd812fe5b7d268fb
dinhngocquanghuy <dinhngocquanghuy@gmail.com>
2019-12-30 13:32:40 +0700

    collide with bone

---
+++ scripts/function.js
@@ -0,0 +1,96 @@
+/**
+ * (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+ */
+
+//==============================================================================
+// Welcome to scripting in Spark AR Studio! Helpful links:
+//
+// Scripting Basics - https://fb.me/spark-scripting-basics
+// Reactive Programming - https://fb.me/spark-reactive-programming
+// Scripting Object Reference - https://fb.me/spark-scripting-reference
+// Changelogs - https://fb.me/spark-changelog
+//==============================================================================
+
+// How to load in modules
+const Scene = require('Scene');
+const Patches = require('Patches');
+const Time = require('Time');
+
+// Use export keyword to make a symbol available in scripting debug console
+export const Diagnostics = require('Diagnostics');
+
+var X = 0;
+var Y = -250;
+Patches.setScalarValue('posX', X);
+Patches.setScalarValue('posY', Y);
+// ------------------------- //
+
+export function changeXRight(moveUp,moveLeft) {
+    const posXChange = Time.setInterval(function () {
+        X = X + 5;
+        Patches.setScalarValue('posX', X);
+    }, 200);
+    moveUp.monitor().subscribe(function () {
+        if(moveUp.pinLastValue()){
+            Time.clearInterval(posXChange);
+        }
+    });
+    moveLeft.monitor().subscribe(function () {
+        if(moveLeft.pinLastValue()){
+            Time.clearInterval(posXChange);
+        }
+    });
+}
+export function changeXLeft(moveUp,moveRight) {
+    const posXChange = Time.setInterval(function () {
+        X = X - 5;
+        Patches.setScalarValue('posX', X);
+    }, 200);
+    moveUp.monitor().subscribe(function () {
+        if(moveUp.pinLastValue()){
+            Time.clearInterval(posXChange);
+        }
+    });
+    moveRight.monitor().subscribe(function () {
+        if(moveRight.pinLastValue()){
+            Time.clearInterval(posXChange);
+        }
+    });
+}
+export function changeY(moveLeft,moveRight) {
+    const posYChange = Time.setInterval(function () {
+        Y = Y + 5;
+        Patches.setScalarValue('posY', Y);
+    }, 200);
+
+    moveRight.monitor().subscribe(function () {
+        if(moveRight.pinLastValue()){
+            Time.clearInterval(posYChange);
+        }
+    });
+    moveLeft.monitor().subscribe(function () {
+        if(moveLeft.pinLastValue()){
+            Time.clearInterval(posYChange);
+        }
+    });
+}
+
+export function CollideFishBone() {
+
+}
+// ------------------------- //
+
+// To use variables and functions across files, use export/import keyword
+// export const animationDuration = 10;
+
+// Use import keyword to import a symbol from another file
+// import { animationDuration } from './script.js'
+
+// To access scene objects
+// const directionalLight = Scene.root.find('directionalLight0');
+
+// To access class properties
+// const directionalLightIntensity = directionalLight.intensity;
+
+// To log messages to the console
+// Diagnostics.log('Console message logged from the script.');
--- scripts/script.js
+++ scripts/script.js
@@ -18,86 +18,31 @@ const Time = require('Time');
 const Patches = require('Patches');
 
 // ------------------------- //
+const {
+  changeXLeft,changeXRight,changeY
+} = require("./function.js");
 
-var X = 0;
-var Y = -250;
 const moveLeft = Patches.getBooleanValue('moveLeft');
 const moveRight = Patches.getBooleanValue('moveRight');
 const moveUp = Patches.getBooleanValue('moveUp');
 
 // ------------------------- //
 
-Patches.setScalarValue('posX', X);
-Patches.setScalarValue('posY', Y);
-
-// ------------------------- //
-
-function changeXRight() {
-    const posXChange = Time.setInterval(function () {
-        X = X + 5;
-        Patches.setScalarValue('posX', X);
-    }, 200);
-    moveUp.monitor().subscribe(function () {
-        if(moveUp.pinLastValue()){
-            Time.clearInterval(posXChange);
-        }
-    });
-    moveLeft.monitor().subscribe(function () {
-        if(moveLeft.pinLastValue()){
-            Time.clearInterval(posXChange);
-        }
-    });
-}
-function changeXLeft() {
-    const posXChange = Time.setInterval(function () {
-        X = X - 5;
-        Patches.setScalarValue('posX', X);
-    }, 200);
-    moveUp.monitor().subscribe(function () {
-        if(moveUp.pinLastValue()){
-            Time.clearInterval(posXChange);
-        }
-    });
-    moveRight.monitor().subscribe(function () {
-        if(moveRight.pinLastValue()){
-            Time.clearInterval(posXChange);
-        }
-    });
-}
-function changeY() {
-    const posYChange = Time.setInterval(function () {
-        Y = Y + 5;
-        Patches.setScalarValue('posY', Y);
-    }, 200);
-
-    moveRight.monitor().subscribe(function () {
-        if(moveRight.pinLastValue()){
-            Time.clearInterval(posYChange);
-        }
-    });
-    moveLeft.monitor().subscribe(function () {
-        if(moveLeft.pinLastValue()){
-            Time.clearInterval(posYChange);
-        }
-    });
-}
-
-// ------------------------- //
 
 
 moveUp.monitor().subscribe(function () {
     if(moveUp.pinLastValue()){
-        changeY();
+        changeY(moveLeft,moveRight);
     }
 
 });
 moveRight.monitor().subscribe(function () {
     if(moveRight.pinLastValue()){
-        changeXRight();
+        changeXRight(moveUp,moveLeft);
     }
 });
 moveLeft.monitor().subscribe(function () {
     if(moveLeft.pinLastValue()){
-        changeXLeft();
+        changeXLeft(moveUp,moveRight);
     }
 });
